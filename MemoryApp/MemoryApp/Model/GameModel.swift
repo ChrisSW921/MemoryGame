82eb972095d37da3915dd1d808d347ccbe8aba46
tdcoder17 <57919329+tdcoder17@users.noreply.github.com>
2021-02-10 16:08:52 -0600

    GameModel update

---
+++ MemoryApp/MemoryApp/Model/GameModel.swift
@@ -0,0 +1,17 @@
+//
+//  GameModel.swift
+//  MemoryApp
+//
+//  Created by Theo Davis on 2/10/21.
+//
+
+import Foundation
+
+class Game {
+    
+    let score: Int
+    
+    init(score: Int) {
+        self.score = score
+    }
+}//end of class
