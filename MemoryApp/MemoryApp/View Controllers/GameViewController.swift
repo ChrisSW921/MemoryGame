1081035539c6ae1ff319158284811e925bf9775e
codedubu <mccaine.river@gmail.com>
2021-02-10 14:13:14 -0800

    Adds GameViewController with function calls set up.

---
+++ MemoryApp/MemoryApp/View Controllers/GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  MemoryApp
+//
+//  Created by River McCaine on 2/10/21.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func unlockInterface() {
+        
+    }
+    
+    func updateLevelFormula() {
+        
+    }
+    
+    func animateButtonFlashes() {
+        
+    }
+    
+    func hideNextLevelButton() {
+        
+    }
+    
+    func fadePlayerInstructions() {
+        
+    }
+
+} // END OF CLASS 
