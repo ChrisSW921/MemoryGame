bd9f5252d321aaff28e0718eebf778bda57fa800
Chris <withscottc921@gmail.com>
2021-02-11 09:43:50 -0700

    Added function to present level and protocol

--- MemoryApp/MemoryApp/Controllers/Model Controller/GameController.swift
+++ MemoryApp/MemoryApp/Controllers/Model Controller/GameController.swift
@@ -7,20 +7,31 @@
 
 import Foundation
 
+protocol PresentLevelDelegate: AnyObject {
+    func presentLevel()
+}
+
 class GameController {
     
     // MARK: - Properties
     static let shared = GameController()
     var correctSequence: [Int] = []
     var guessedSequence: [Int] = []
+    var currentLevel: Int = 1
+    weak var delegate: PresentLevelDelegate?
     
     // MARK: - Methods
     func didTapSquare() {
         
     }
     
-    func presentLevel() {
-        
+    func presentLevel(currentLevel: Int) {
+        correctSequence = []
+        guessedSequence = []
+        for num in 0..<currentLevel {
+            correctSequence.append(Int.random(in: 1...4))
+        }
+        delegate?.presentLevel()
     }
     
     func nextLevel() {
@@ -29,6 +40,10 @@ class GameController {
     
     func saveHighScore() {
         
+    }
+    
+    func didBeatLevel() -> Bool {
+        return correctSequence.count == guessedSequence.count
     }
     
 }//End of Class
