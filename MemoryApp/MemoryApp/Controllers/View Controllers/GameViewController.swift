6d72051d4dab3874305fc6bb9abc2ed144b5a696
codedubu <mccaine.river@gmail.com>
2021-02-11 09:10:22 -0800

    Adds Game View and connects outlets to GameViewController.

--- MemoryApp/MemoryApp/Controllers/View Controllers/GameViewController.swift
+++ MemoryApp/MemoryApp/Controllers/View Controllers/GameViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var currentLevelLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    @IBOutlet weak var watchCloselyLabel: UILabel!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var purpleButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    // MARK: - Actions
+    @IBAction func startGameButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func gameButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Game Functions
     func unlockInterface() {
         
     }
